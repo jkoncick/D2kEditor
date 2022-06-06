f8680b7b71f4780cbfeb64879aed9de8f241ec4a
Hisymak <kozten@seznam.cz>
2022-06-06 22:26:16 +0200

    Increase version, update changelog

--- main.dfm
+++ main.dfm
@@ -102,7 +102,7 @@ object MainWindow: TMainWindow
         Width = 150
       end
       item
-        Text = 'v2.1'
+        Text = 'v2.11'
         Width = 0
       end>
   end
--- main.pas
+++ main.pas
@@ -1246,8 +1246,8 @@ begin
   ShowMessage('Dune 2000 Map and Mission Editor'#13#13+
               'Part of D2K+ Editing tools'#13#13+
               'Made by Klofkac (kozten@seznam.cz)'#13+
-              'Version 2.1'#13+
-              'Date: 2022-05-01'#13#13+
+              'Version 2.11'#13+
+              'Date: 2022-06-06'#13#13+
               'http://github.com/jkoncick/D2kEditor'#13#13+
               'Special thanks to:'#13+
               'mvi - for making the original Mission editor'#13+
